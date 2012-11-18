b5f2f114f1c0be54a070a58d04b4d594ed887035
Michael Oswalt <mao47@case.edu>
2012-11-18 17:23:12 -0500

    outline of shootRay method

--- CastleOswaltAS7/main.cpp
+++ CastleOswaltAS7/main.cpp
@@ -260,123 +260,6 @@ void layoutReader(char *filename)
 }
 
 
-int verts, faces, norms;    // Number of vertices, faces and normals in the system
-point *vertList, *normList; // Vertex and Normal Lists
-faceStruct *faceList;	    // Face List
-
-// The mesh reader itself
-// It can read *very* simple obj files
-void meshReader (char *filename,int sign)
-{
-  float x,y,z,len;
-  int i;
-  char letter;
-  point v1,v2,crossP;
-  int ix,iy,iz;
-  int *normCount;
-  FILE *fp;
-
-  fp = fopen(filename, "r");
-  if (fp == NULL) { 
-    printf("Cannot open %s\n!", filename);
-    exit(0);
-  }
-
-  // Count the number of vertices and faces
-  while(!feof(fp))
-    {
-      fscanf(fp,"%c %f %f %f\n",&letter,&x,&y,&z);
-      if (letter == 'v')
-	{
-	  verts++;
-	}
-      else
-	{
-	  faces++;
-	}
-    }
-
-  fclose(fp);
-
-  printf("verts : %d\n", verts);
-  printf("faces : %d\n", faces);
-
-  // Dynamic allocation of vertex and face lists
-  faceList = (faceStruct *)malloc(sizeof(faceStruct)*faces);
-  vertList = (point *)malloc(sizeof(point)*verts);
-  normList = (point *)malloc(sizeof(point)*verts);
-
-  fp = fopen(filename, "r");
-
-  // Read the veritces
-  for(i = 0;i < verts;i++)
-    {
-      fscanf(fp,"%c %f %f %f\n",&letter,&x,&y,&z);
-      vertList[i].x = x;
-      vertList[i].y = y;
-      vertList[i].z = z;
-    }
-
-  // Read the faces
-  for(i = 0;i < faces;i++)
-    {
-      fscanf(fp,"%c %d %d %d\n",&letter,&ix,&iy,&iz);
-      faceList[i].v1 = ix - 1;
-      faceList[i].v2 = iy - 1;
-      faceList[i].v3 = iz - 1;
-    }
-  fclose(fp);
-
-
-  // The part below calculates the normals of each vertex
-  normCount = (int *)malloc(sizeof(int)*verts);
-  for (i = 0;i < verts;i++)
-    {
-      normList[i].x = normList[i].y = normList[i].z = 0.0;
-      normCount[i] = 0;
-    }
-
-  for(i = 0;i < faces;i++)
-    {
-      v1.x = vertList[faceList[i].v2].x - vertList[faceList[i].v1].x;
-      v1.y = vertList[faceList[i].v2].y - vertList[faceList[i].v1].y;
-      v1.z = vertList[faceList[i].v2].z - vertList[faceList[i].v1].z;
-      v2.x = vertList[faceList[i].v3].x - vertList[faceList[i].v2].x;
-      v2.y = vertList[faceList[i].v3].y - vertList[faceList[i].v2].y;
-      v2.z = vertList[faceList[i].v3].z - vertList[faceList[i].v2].z;
-
-      crossP.x = v1.y*v2.z - v1.z*v2.y;
-      crossP.y = v1.z*v2.x - v1.x*v2.z;
-      crossP.z = v1.x*v2.y - v1.y*v2.x;
-
-      len = sqrt(crossP.x*crossP.x + crossP.y*crossP.y + crossP.z*crossP.z);
-
-      crossP.x = -crossP.x/len;
-      crossP.y = -crossP.y/len;
-      crossP.z = -crossP.z/len;
-
-      normList[faceList[i].v1].x = normList[faceList[i].v1].x + crossP.x;
-      normList[faceList[i].v1].y = normList[faceList[i].v1].y + crossP.y;
-      normList[faceList[i].v1].z = normList[faceList[i].v1].z + crossP.z;
-      normList[faceList[i].v2].x = normList[faceList[i].v2].x + crossP.x;
-      normList[faceList[i].v2].y = normList[faceList[i].v2].y + crossP.y;
-      normList[faceList[i].v2].z = normList[faceList[i].v2].z + crossP.z;
-      normList[faceList[i].v3].x = normList[faceList[i].v3].x + crossP.x;
-      normList[faceList[i].v3].y = normList[faceList[i].v3].y + crossP.y;
-      normList[faceList[i].v3].z = normList[faceList[i].v3].z + crossP.z;
-      normCount[faceList[i].v1]++;
-      normCount[faceList[i].v2]++;
-      normCount[faceList[i].v3]++;
-    }
-  for (i = 0;i < verts;i++)
-    {
-      normList[i].x = (float)sign*normList[i].x / (float)normCount[i];
-      normList[i].y = (float)sign*normList[i].y / (float)normCount[i];
-      normList[i].z = (float)sign*normList[i].z / (float)normCount[i];
-    }
-
-}
-
 void drawRect(double x, double y, double w, double h)
 {
 	glVertex2f(x,y);
@@ -385,6 +268,46 @@ void drawRect(double x, double y, double w, double h)
 	glVertex2f(x, y+h);
 }
 
+
+float shootRay(ray myRay, int depth)
+{
+	//get intersections
+	double distance;
+	point intersection, normal;
+	//spheres
+	int i = 0;
+	for( i = 0; i < sphereCount; i++)
+	{
+		point tempint, tempnorm;
+		double tempdist;
+		if(sphereList[i].intersects(myRay, &tempint, &tempnorm, &tempdist)
+		{
+			if(tempdist < distance && tempdist > 0)
+			{
+				distance = tempdist;
+				intersection = tempint;
+				normal = tempnorm;
+			}
+		}
+	}
+	//meshes
+	//select closest point and object
+	if(didIntersect == 0)
+		return 0;
+
+	//get normal
+	//rgb = localIllumination()
+
+	depth --;
+	if(depth > 0)
+	{
+		ray reflected = calcReflectedRay();
+		ray refracted = calcRefractedRay();
+
+		//return weigthed sum of reflected + refracted + local
+	}
+
+}
 
 // The display function. It is called whenever the window needs
 // redrawing (ie: overlapping window moves, resize, maximize)
